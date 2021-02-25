58d270fd328dbfcddbf50b22264001d27a0cf66a
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2021-02-25 01:39:57 +0100

    fixed wrong response message

--- src/main/java/de/netbeacon/xenia/bot/commands/structure/tags/HYBRIDTag.java
+++ src/main/java/de/netbeacon/xenia/bot/commands/structure/tags/HYBRIDTag.java
@@ -52,7 +52,7 @@ public class HYBRIDTag extends HybridCommand {
         CmdArg<String> tagA = cmdArgs.getByIndex(0);
         try{
             Tag tag = tagCache.get(tagA.getValue());
-            commandEvent.getEvent().getChannel().sendMessage(translationPackage.getTranslationWithPlaceholders(getClass(), "response.error.msg", tag.getTagContent(), tag.getId())).queue();
+            commandEvent.getEvent().getChannel().sendMessage(translationPackage.getTranslationWithPlaceholders(getClass(), "response.success.msg", tag.getTagContent(), tag.getId())).queue();
         }catch (CacheException e){
             commandEvent.getEvent().getChannel().sendMessage(onError(translationPackage, translationPackage.getTranslationWithPlaceholders(getClass(), "response.error.msg", tagA.getValue()))).queue();
         }
