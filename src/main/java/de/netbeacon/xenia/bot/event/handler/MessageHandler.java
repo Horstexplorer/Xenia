97d008febad8c1a541a2b3b2912920030105a7c6
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2021-08-08 00:57:42 +0200

    fixed possible regex issue by escaping the input
    Signed-off-by: Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>

--- src/main/java/de/netbeacon/xenia/bot/event/handler/MessageHandler.java
+++ src/main/java/de/netbeacon/xenia/bot/event/handler/MessageHandler.java
@@ -34,6 +34,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static de.netbeacon.xenia.bot.utils.statics.pattern.StaticPattern.ArgPattern;
 
@@ -79,7 +80,7 @@ public class MessageHandler{
 				toolBundle.levelPointManager().feed(bMember);
 				// get the message & check prefix
 				String msg = event.getMessage().getContentRaw();
-				if(!msg.startsWith(bGuild.getPrefix())){
+				if(!msg.startsWith(Pattern.quote(bGuild.getPrefix()))){
 					return;
 				}
 				// check if xenia is not active or inactive in which case we dont do anything
